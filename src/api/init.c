e595ce9025ea20240eaa85114ef715b1b6c4326e
simylein <67859356+simylein@users.noreply.github.com>
2025-05-15 18:40:13 +0200

    feat(init): initialise all tables on demand

---
+++ src/api/init.c
@@ -0,0 +1,48 @@
+#include "../lib/logger.h"
+#include "device/device.h"
+#include "user-device/user-device.h"
+#include "user/user.h"
+#include <sqlite3.h>
+#include <stdlib.h>
+
+int init_table(sqlite3 *database, const char *table, const char *schema) {
+	int status;
+	sqlite3_stmt *stmt;
+
+	debug("%s\n", schema);
+
+	if (sqlite3_prepare_v2(database, schema, -1, &stmt, NULL) != SQLITE_OK) {
+		error("failed to prepare statement because %s\n", sqlite3_errmsg(database));
+		status = -1;
+		goto cleanup;
+	}
+
+	if (sqlite3_step(stmt) != SQLITE_DONE) {
+		error("failed to execute statement because %s\n", sqlite3_errmsg(database));
+		status = -1;
+		goto cleanup;
+	}
+
+	info("created table %s\n", table);
+	status = 0;
+
+cleanup:
+	sqlite3_finalize(stmt);
+	return status;
+}
+
+int init(sqlite3 *database) {
+	if (init_table(database, user_table, user_schema) == -1) {
+		return -1;
+	}
+
+	if (init_table(database, user_device_table, user_device_schema) == -1) {
+		return -1;
+	}
+
+	if (init_table(database, device_table, device_schema) == -1) {
+		return -1;
+	}
+
+	return 0;
+}
---
+++ src/api/init.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <sqlite3.h>
+
+int init(sqlite3 *database);
