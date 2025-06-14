5cd125c7d2e029543d2fd5cff0242bb8c32601af
simylein <67859356+simylein@users.noreply.github.com>
2025-06-14 21:50:56 +0200

    feat(endian): support all the byte swaps

--- src/lib/endian.c
+++ src/lib/endian.c
@@ -1,18 +1,22 @@
 #include <arpa/inet.h>
 #include <stdint.h>
 
-uint64_t hton64(uint64_t value) {
-	uint32_t high_bits = htonl((uint32_t)(value >> 32));
-	uint32_t low_bits = htonl((uint32_t)(value & 0xffffffff));
-	return (((uint64_t)low_bits) << 32) | high_bits;
-}
-
-uint64_t ntoh64(uint64_t value) {
-	uint32_t high_bits = ntohl((uint32_t)(value >> 32));
-	uint32_t low_bits = ntohl((uint32_t)(value & 0xffffffff));
-	return (((uint64_t)low_bits) << 32) | high_bits;
-}
-
 uint16_t hton16(uint16_t value) { return htons(value); }
 
 uint16_t ntoh16(uint16_t value) { return ntohs(value); }
+
+uint32_t hton32(uint32_t value) { return htonl(value); }
+
+uint32_t ntoh32(uint32_t value) { return ntohl(value); }
+
+uint64_t hton64(uint64_t value) {
+	uint32_t upper = htonl((uint32_t)(value >> 32));
+	uint32_t lower = htonl((uint32_t)(value & 0xffffffff));
+	return (((uint64_t)lower) << 32) | upper;
+}
+
+uint64_t ntoh64(uint64_t value) {
+	uint32_t upper = ntohl((uint32_t)(value >> 32));
+	uint32_t lower = ntohl((uint32_t)(value & 0xffffffff));
+	return (((uint64_t)lower) << 32) | upper;
+}
--- src/lib/endian.h
+++ src/lib/endian.h
@@ -2,8 +2,11 @@
 
 #include <stdint.h>
 
+uint16_t hton16(uint16_t value);
+uint16_t ntoh16(uint16_t value);
+
+uint32_t hton32(uint32_t value);
+uint32_t ntoh32(uint32_t value);
+
 uint64_t hton64(uint64_t value);
 uint64_t ntoh64(uint64_t value);
-
-uint16_t hton16(uint16_t value);
-uint16_t hton16(uint16_t value);
