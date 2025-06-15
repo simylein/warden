8635cc7eb9901e8a6c0ca2258b36224c72b85965
simylein <67859356+simylein@users.noreply.github.com>
2025-06-15 21:00:58 +0200

    fix(schema): reading and metric should be one to one

--- src/api/metric.c
+++ src/api/metric.c
@@ -4,6 +4,6 @@ const char *metric_schema = "create table metric ("
 														"photovoltaic real not null, "
 														"battery real not null, "
 														"captured_at timestamp not null, "
-														"uplink_id blob not null, "
+														"uplink_id blob not null unique, "
 														"foreign key (uplink_id) references uplink(id) on delete cascade"
 														")";
--- src/api/reading.c
+++ src/api/reading.c
@@ -11,7 +11,7 @@ const char *reading_schema = "create table reading ("
 														 "temperature real not null, "
 														 "humidity real not null, "
 														 "captured_at timestamp not null, "
-														 "uplink_id blob not null, "
+														 "uplink_id blob not null unique, "
 														 "foreign key (uplink_id) references uplink(id) on delete cascade"
 														 ")";
 
