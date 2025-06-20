519f8c844c480e2c8e3d7a2d52e4314b75814e5c
simylein <67859356+simylein@users.noreply.github.com>
2025-06-20 18:43:49 +0200

    fix(timestamp): also modularize timestamping

---
+++ src/app/timestamp.js
@@ -0,0 +1,13 @@
+const timestamp = (time) => {
+	const date = new Date(Number(time) * 1000);
+
+	const year = date.getFullYear();
+	const month = (date.getMonth() + 1).toString().padStart(2, '0');
+	const day = date.getDate().toString().padStart(2, '0');
+
+	const hour = date.getHours().toString().padStart(2, '0');
+	const minute = date.getMinutes().toString().padStart(2, '0');
+	const second = date.getSeconds().toString().padStart(2, '0');
+
+	return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+};
