f9d264b2f48d3ee2b9dd07fa67d8c57dd7799568
simylein <67859356+simylein@users.noreply.github.com>
2025-06-20 20:40:33 +0200

    refactor(assemble): do not keep inject comments

--- src/app/assemble.c
+++ src/app/assemble.c
@@ -9,107 +9,73 @@
 #include <unistd.h>
 
 int assemble(file_t *asset) {
-	inject_t injects[16];
-	uint8_t injects_len = 0;
-
 	bool inject = false;
 
-	uint8_t stage = 0;
-	size_t index = 0;
-
-	size_t script_index;
-	while (stage == 0 && index < asset->len) {
-		if (index > 14 && memcmp(&asset->ptr[index - 8], "<script>", 8) == 0) {
-			script_index = index;
-			stage = 1;
-		}
-		index++;
-	}
-
-	while (stage == 1 && index < asset->len) {
-		char *byte = &asset->ptr[index];
-		if (inject == false && index > 17 && memcmp(&asset->ptr[index - 11], "// @inject ", 11) == 0) {
-			injects[injects_len].ptr = &asset->ptr[index];
-			injects[injects_len].len = 0;
-			inject = true;
-		} else if (inject == true && *byte == '\n') {
-			injects[injects_len].len += 1;
-			injects_len += 1;
-			if (injects_len >= sizeof(injects) / sizeof(inject_t)) {
-				error("can not assemble more than %zu files\n", sizeof(injects) / sizeof(inject_t));
-				return -1;
-			}
-			inject = false;
-		} else if (inject == true) {
-			injects[injects_len].len += 1;
-		} else if (index > 15 && memcmp(&asset->ptr[index - 9], "</script>", 9) == 0) {
-			stage = 2;
-		}
-		index++;
-	}
-
-	if (stage == 0) {
-		return 0;
-	}
-
-	if (stage != 2) {
-		error("file %s contains an invalid script tag\n", asset->path);
-		return -1;
-	}
-
-	file_t files[16];
-	uint8_t files_len = 0;
-
-	for (uint8_t ind = 0; ind < injects_len; ind++) {
-		char path[65];
-		if (injects[ind].len > sizeof(path) - 1) {
-			error("inject %.*s exceeds buffer size %zu\n", injects[ind].len, injects[ind].ptr, sizeof(path) - 1);
-			return -1;
-		}
-		files[ind].fd = -1;
-		files[ind].ptr = NULL;
-		sprintf(path, "%.*s", injects[ind].len, injects[ind].ptr);
-		files[ind].path = path;
-		if (file(&files[files_len]) == -1) {
-			return -1;
-		}
-		files_len += 1;
-	}
+	size_t asset_index = 0;
+	size_t assemble_index = 0;
 
 	size_t assemble_len = asset->len;
-	for (uint8_t ind = 0; ind < files_len; ind++) {
-		assemble_len += files[ind].len;
-	}
-
 	char *assemble_ptr = malloc(assemble_len);
 	if (assemble_ptr == NULL) {
 		error("failed to allocate %zu bytes for %s because %s\n", assemble_len, asset->path, errno_str());
 		return -1;
 	}
 
-	size_t asset_index = 0;
-	size_t assemble_index = 0;
+	while (asset_index < asset->len) {
+		char *byte = &asset->ptr[asset_index];
 
-	memcpy(&assemble_ptr[assemble_index], &asset->ptr[asset_index], script_index);
-	asset_index += script_index;
-	assemble_index += script_index;
+		if (inject == false && asset_index + 11 < asset->len && memcmp(byte, "// @inject ", 11) == 0) {
+			inject = true;
+		}
 
-	for (uint8_t ind = 0; ind < files_len; ind++) {
-		memcpy(&assemble_ptr[assemble_index], files[ind].ptr, files[ind].len);
-		assemble_index += files[ind].len;
+		if (inject == true) {
+			char path[65];
+			uint8_t path_len = 0;
+			byte += 11;
+			asset_index += 11;
+			char *start = byte;
+			while (asset_index < asset->len) {
+				if (*byte == '\n' || path_len >= sizeof(path) - 1) {
+					inject = false;
+					break;
+				}
+				byte += 1;
+				path_len += 1;
+				asset_index += 1;
+			}
+			sprintf(path, "%.*s", path_len, start);
+
+			file_t assemble;
+			assemble.fd = -1;
+			assemble.ptr = NULL;
+			assemble.path = path;
+
+			if (file(&assemble) == -1) {
+				return -1;
+			}
+
+			assemble_len += assemble.len;
+			assemble_ptr = realloc(assemble_ptr, assemble_len);
+			if (assemble_ptr == NULL) {
+				error("failed to allocate %zu bytes for %s because %s\n", assemble_len, asset->path, errno_str());
+				return -1;
+			}
+
+			memcpy(&assemble_ptr[assemble_index], assemble.ptr, assemble.len);
+			assemble_index += assemble.len;
+
+			close(assemble.fd);
+			free(assemble.ptr);
+		} else {
+			assemble_ptr[assemble_index] = *byte;
+			assemble_index += 1;
+		}
+
+		asset_index += 1;
 	}
-
-	memcpy(&assemble_ptr[assemble_index], &asset->ptr[asset_index], asset->len - script_index);
-	asset_index += asset->len - script_index;
-	assemble_index += asset->len - script_index;
 
 	asset->ptr = assemble_ptr;
 	asset->len = assemble_len;
-
-	for (uint8_t ind = 0; ind < files_len; ind++) {
-		close(files[ind].fd);
-		free(files[ind].ptr);
-	}
 
 	return 0;
 }
