c02d86a09a4f79acbaec99e636bbb79ef973dfc7
simylein <67859356+simylein@users.noreply.github.com>
2025-07-07 19:30:54 +0200

    chore(color-uplink): also check for type equals

--- src/app/color-uplink.js
+++ src/app/color-uplink.js
@@ -34,17 +34,17 @@ const colorSnr = (element, snr) => {
 
 const colorSf = (element, sf) => {
 	switch (true) {
-		case sf == 7:
+		case sf === 7:
 			return element.classList.add('text-green-600', 'dark:text-green-400', 'background-green-50', 'dark:background-green-950');
-		case sf == 8:
+		case sf === 8:
 			return element.classList.add('text-lime-600', 'dark:text-lime-400', 'background-lime-50', 'dark:background-lime-950');
-		case sf == 9:
+		case sf === 9:
 			return element.classList.add('text-yellow-600', 'dark:text-yellow-400', 'background-yellow-50', 'dark:background-yellow-950');
-		case sf == 10:
+		case sf === 10:
 			return element.classList.add('text-amber-600', 'dark:text-amber-400', 'background-amber-50', 'dark:background-amber-950');
-		case sf == 11:
+		case sf === 11:
 			return element.classList.add('text-orange-600', 'dark:text-orange-400', 'background-orange-50', 'dark:background-orange-950');
-		case sf == 12:
+		case sf === 12:
 			return element.classList.add('text-red-600', 'dark:text-red-400', 'background-red-50', 'dark:background-red-950');
 	}
 };
