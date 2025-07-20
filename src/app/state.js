8c1f28080a886bdf7bfd4f54fea1c1480e14a878
simylein <67859356+simylein@users.noreply.github.com>
2025-07-20 14:24:25 +0200

    feat(state): support add remove text and sizes on error

--- src/app/state.js
+++ src/app/state.js
@@ -13,8 +13,8 @@ const loading = (element, add, remove, text, height, width) => {
 };
 
 const paint = (element, add, remove, text, height, width) => {
-	element.classList.add('w-fit', ...add);
-	element.classList.remove('background-neutral-200', 'dark:background-neutral-800', ...remove);
+	element.classList.add(...add, 'w-fit');
+	element.classList.remove('background-neutral-200', 'dark:background-neutral-800', 'background-red-200', 'dark:background-red-800', ...remove);
 	if (text !== undefined && text !== null) {
 		element.innerText = text;
 	}
@@ -26,7 +26,16 @@ const paint = (element, add, remove, text, height, width) => {
 	}
 };
 
-const error = (element) => {
-	element.classList.add('background-red-200', 'dark:background-red-800');
-	element.classList.remove('background-neutral-200', 'dark:background-neutral-800');
+const error = (element, add, remove, text, height, width) => {
+	element.classList.add(...add, 'background-red-200', 'dark:background-red-800');
+	element.classList.remove('w-fit', 'background-neutral-200', 'dark:background-neutral-800', ...remove);
+	if (text !== undefined && text !== null) {
+		element.innerText = text;
+	}
+	if (height !== undefined && height !== null) {
+		element.style.height = `${height}%`;
+	}
+	if (width !== undefined && width !== null) {
+		element.style.width = `${width}%`;
+	}
 };
