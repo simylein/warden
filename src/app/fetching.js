e90d4a7376718abd0446f2ed17cc7ba32be03054
simylein <67859356+simylein@users.noreply.github.com>
2025-08-04 11:42:24 +0200

    feat(fetching): generic fetching with retries

---
+++ src/app/fetching.js
@@ -0,0 +1,35 @@
+const fetching = (context, loading, fetch, parse, paint, error) => {
+	const load = async (silent) => {
+		if (context.timeout) {
+			clearTimeout(context.timeout);
+			context.timeout = null;
+		}
+		if (context.controller) {
+			context.controller.abort();
+		}
+		context.controller = new AbortController();
+		if (!silent) {
+			loading(context);
+		}
+		try {
+			const response = await fetch(context);
+			await parse(context, response);
+			context.retries = 0;
+			paint(context);
+			if (context.reload !== null) {
+				context.timeout = setTimeout(() => load(true), context.reload * 1000);
+			}
+		} catch (err) {
+			if (err?.name !== 'AbortError') {
+				error(context);
+				if (context.retries < 4) {
+					context.timeout = setTimeout(() => {
+						context.retries++;
+						load();
+					}, 2 ** context.retries * 1000);
+				}
+			}
+		}
+	};
+	return load;
+};
