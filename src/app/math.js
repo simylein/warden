cdfec33260260db250d57e7bf7498b64a0d0670b
simylein <67859356+simylein@users.noreply.github.com>
2025-08-05 16:19:49 +0200

    chore(math): cap at 1 unit steps

--- src/app/math.js
+++ src/app/math.js
@@ -8,12 +8,8 @@ const floor = (value, range) => {
 			return Math.floor(value * 2) / 2;
 		case range <= 10:
 			return Math.floor(value);
-		case range <= 20:
-			return Math.floor(value / 2) * 2;
-		case range <= 50:
-			return Math.floor(value / 5) * 5;
-		case range > 50:
-			return Math.floor(value / 10) * 10;
+		case range > 10:
+			return Math.floor(value);
 	}
 };
 
@@ -27,11 +23,7 @@ const ceil = (value, range) => {
 			return Math.ceil(value * 2) / 2;
 		case range <= 10:
 			return Math.ceil(value);
-		case range <= 20:
-			return Math.ceil(value / 2) * 2;
-		case range <= 50:
-			return Math.ceil(value / 5) * 5;
-		case range > 50:
-			return Math.ceil(value / 10) * 10;
+		case range > 10:
+			return Math.ceil(value);
 	}
 };
