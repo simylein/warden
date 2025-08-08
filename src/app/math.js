67120bd238328a6f353beeeebcf12517c20086d2
simylein <67859356+simylein@users.noreply.github.com>
2025-08-08 11:47:43 +0200

    fix(math): quadruple range thresholds

--- src/app/math.js
+++ src/app/math.js
@@ -1,29 +1,25 @@
 const floor = (value, range) => {
 	switch (true) {
-		case range <= 1:
+		case range <= 4:
 			return Math.floor(value * 10) / 10;
-		case range <= 2:
+		case range <= 8:
 			return Math.floor(value * 5) / 5;
-		case range <= 5:
+		case range <= 20:
 			return Math.floor(value * 2) / 2;
-		case range <= 10:
-			return Math.floor(value);
-		case range > 10:
+		case range > 20:
 			return Math.floor(value);
 	}
 };
 
 const ceil = (value, range) => {
 	switch (true) {
-		case range <= 1:
+		case range <= 4:
 			return Math.ceil(value * 10) / 10;
-		case range <= 2:
+		case range <= 8:
 			return Math.ceil(value * 5) / 5;
-		case range <= 5:
+		case range <= 20:
 			return Math.ceil(value * 2) / 2;
-		case range <= 10:
-			return Math.ceil(value);
-		case range > 10:
+		case range > 20:
 			return Math.ceil(value);
 	}
 };
