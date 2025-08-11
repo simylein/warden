7b9e387dcccef643ac9cfaae3e9631683e47aa35
simylein <67859356+simylein@users.noreply.github.com>
2025-08-11 17:26:10 +0200

    fix(breakpoint): adjust width returned

--- src/app/breakpoint.js
+++ src/app/breakpoint.js
@@ -1,14 +1,16 @@
 const breakpoint = () => {
 	switch (true) {
-		case window.innerWidth < 640:
+		case window.innerWidth < 512:
 			return 100;
-		case window.innerWidth >= 640 && window.innerWidth < 768:
+		case window.innerWidth >= 512 && window.innerWidth < 640:
 			return 150;
-		case window.innerWidth >= 768 && window.innerWidth < 1024:
+		case window.innerWidth >= 640 && window.innerWidth < 768:
 			return 200;
-		case window.innerWidth >= 1024 && window.innerWidth < 1280:
-			return 250;
-		case window.innerWidth >= 1280:
+		case window.innerWidth >= 768 && window.innerWidth < 1024:
 			return 300;
+		case window.innerWidth >= 1024 && window.innerWidth < 1280:
+			return 400;
+		case window.innerWidth >= 1280:
+			return 500;
 	}
 };
