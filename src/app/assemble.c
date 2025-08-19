b43d2de1e1450219ccb66a8f644f32a8fc0e96d0
simylein <67859356+simylein@users.noreply.github.com>
2025-08-19 18:02:05 +0200

    feat(assemble): inject ref component files

--- src/app/assemble.c
+++ src/app/assemble.c
@@ -9,6 +9,9 @@
 #include <unistd.h>
 
 int assemble(file_t *asset) {
+	bool tag = false;
+	bool quote = false;
+
 	size_t asset_ind = 0;
 	size_t assemble_ind = 0;
 
@@ -17,8 +20,84 @@ int assemble(file_t *asset) {
 
 	while (asset_ind < asset->len) {
 		char *byte = &asset->ptr[asset_ind];
+		if (*byte == '<' && !tag && !quote) {
+			if (asset_ind + 7 < asset->len && memcmp(byte + 1, "/body>", 6) == 0) {
+				break;
+			}
+			tag = true;
+		} else if (*byte == '>' && tag && !quote) {
+			tag = false;
+		} else if (*byte == '"' && tag && !quote) {
+			quote = true;
+		} else if (*byte == '"' && tag && quote) {
+			quote = false;
+		}
 
-		if (asset_ind + 8 < asset->len && memcmp(byte, "import '", 8) == 0) {
+		if (tag && !quote && *byte == ' ' && asset_ind + 6 < asset->len && memcmp(byte + 1, "ref=\"", 5) == 0) {
+			char path[65];
+			uint8_t path_len = 0;
+			byte += 6;
+			asset_ind += 6;
+			char *start = byte;
+			while (asset_ind < asset->len) {
+				if (*byte == '"' || path_len >= sizeof(path) - 1) {
+					break;
+				}
+				byte += 1;
+				path_len += 1;
+				asset_ind += 1;
+			}
+			while (asset_ind < asset->len) {
+				if (*byte == '>') {
+					break;
+				}
+				byte += 1;
+				asset_ind += 1;
+			}
+
+			sprintf(path, "%.*s", path_len, start);
+			file_t component = {.fd = -1, .ptr = NULL, .path = path, .lock = PTHREAD_RWLOCK_INITIALIZER};
+			if (file(&component) == -1) {
+				return -1;
+			}
+
+			assemble_len += component.len;
+			assemble_ptr = realloc(assemble_ptr, assemble_len);
+			if (assemble_ptr == NULL) {
+				error("failed to allocate %zu bytes for %s because %s\n", assemble_len, asset->path, errno_str());
+				return -1;
+			}
+
+			while (assemble_ind > 0 && assemble_ptr[assemble_ind] != '<') {
+				byte -= 1;
+				assemble_ind -= 1;
+			}
+
+			memcpy(&assemble_ptr[assemble_ind], component.ptr, component.len);
+			assemble_ind += component.len;
+
+			close(component.fd);
+			free(component.ptr);
+		} else {
+			if (assemble_ind >= assemble_len) {
+				assemble_len += 4096;
+				assemble_ptr = realloc(assemble_ptr, assemble_len);
+				if (assemble_ptr == NULL) {
+					error("failed to allocate %zu bytes for %s because %s\n", assemble_len, asset->path, errno_str());
+					return -1;
+				}
+			}
+			assemble_ptr[assemble_ind] = *byte;
+			assemble_ind += 1;
+		}
+
+		asset_ind += 1;
+	}
+
+	while (asset_ind < asset->len) {
+		char *byte = &asset->ptr[asset_ind];
+
+		if (*byte == 'i' && asset_ind + 8 < asset->len && memcmp(byte + 1, "mport '", 7) == 0) {
 			char path[65];
 			uint8_t path_len = 0;
 			byte += 8;
