d1d565a365d92f9a22ff34c118033babe3210562
simylein <67859356+simylein@users.noreply.github.com>
2025-08-28 16:30:55 +0200

    fix(decode): photovoltaic and battery use voltage divider

--- src/api/decode.c
+++ src/api/decode.c
@@ -30,10 +30,10 @@ int decode_kind_02(uint8_t *data, uint8_t data_len, time_t received_at, metric_t
 	}
 
 	uint16_t photovoltaic_raw = (uint16_t)(data[0] << 4) | (uint16_t)((data[1] >> 4) & 0x0f);
-	metric->photovoltaic = (photovoltaic_raw * 3.3f) / 4095.0f;
+	metric->photovoltaic = (photovoltaic_raw * 3.3f) / 4095.0f * 2;
 
 	uint16_t battery_raw = (uint16_t)((data[1] & 0x0f) << 8) | (uint16_t)data[2];
-	metric->battery = (battery_raw * 3.3f) / 4095.0f;
+	metric->battery = (battery_raw * 3.3f) / 4095.0f * 2;
 
 	metric->captured_at = received_at;
 
@@ -58,10 +58,10 @@ int decode_kind_03(uint8_t *data, uint8_t data_len, time_t received_at, reading_
 	trace("temperature %.2f humidity %.2f captured_at %lu\n", reading->temperature, reading->humidity, reading->captured_at);
 
 	uint16_t photovoltaic_raw = (uint16_t)(data[4] << 4) | (uint16_t)((data[5] >> 4) & 0x0f);
-	metric->photovoltaic = (photovoltaic_raw * 3.3f) / 4095.0f;
+	metric->photovoltaic = (photovoltaic_raw * 3.3f) / 4095.0f * 2;
 
 	uint16_t battery_raw = (uint16_t)((data[5] & 0x0f) << 8) | (uint16_t)data[6];
-	metric->battery = (battery_raw * 3.3f) / 4095.0f;
+	metric->battery = (battery_raw * 3.3f) / 4095.0f * 2;
 
 	metric->captured_at = received_at;
 
