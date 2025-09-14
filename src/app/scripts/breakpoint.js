42349a5af8188eb09f4c54bed86de74bb61681b6
simylein <67859356+simylein@users.noreply.github.com>
2025-09-14 18:25:14 +0200

    fix(breakpoint): reference graph width directly

--- src/app/scripts/breakpoint.js
+++ src/app/scripts/breakpoint.js
@@ -1,24 +1,32 @@
-const breakpoint = () => {
+const breakpoint = (width) => {
 	switch (true) {
-		case window.innerWidth < 384:
-			return 92;
-		case window.innerWidth >= 384 && window.innerWidth < 512:
-			return 158;
-		case window.innerWidth >= 512 && window.innerWidth < 640:
-			return 224;
-		case window.innerWidth >= 640 && window.innerWidth < 768:
-			return 290;
-		case window.innerWidth >= 768 && window.innerWidth < 1024:
-			return 344;
-		case window.innerWidth >= 1024 && window.innerWidth < 1280:
-			return 476;
-		case window.innerWidth >= 1280 && window.innerWidth < 1536:
-			return 596;
-		case window.innerWidth >= 1536 && window.innerWidth < 1792:
-			return 730;
-		case window.innerWidth >= 1792 && window.innerWidth < 2048:
-			return 864;
-		case window.innerWidth >= 2048:
+		case width < 128:
+			return 58;
+		case width >= 128 && width < 192:
+			return 83;
+		case width >= 192 && width < 256:
+			return 116;
+		case width >= 256 && width < 320:
+			return 150;
+		case width >= 320 && width < 384:
+			return 183;
+		case width >= 384 && width < 512:
+			return 233;
+		case width >= 512 && width < 640:
+			return 300;
+		case width >= 640 && width < 768:
+			return 366;
+		case width >= 768 && width < 1024:
+			return 466;
+		case width >= 1024 && width < 1280:
+			return 600;
+		case width >= 1280 && width < 1536:
+			return 733;
+		case width >= 1536 && width < 1792:
+			return 866;
+		case width >= 1792 && width < 2048:
 			return 1000;
+		case width >= 2048:
+			return 1133;
 	}
 };
