695f83881235d456075a225b600190031884d133
simylein <67859356+simylein@users.noreply.github.com>
2025-10-02 17:26:12 +0200

    feat(math): support scaling down further

--- src/app/scripts/math.js
+++ src/app/scripts/math.js
@@ -1,5 +1,11 @@
 const floor = (value, range) => {
 	switch (true) {
+		case range <= 0.4:
+			return Math.floor(value * 100) / 100;
+		case range <= 0.8:
+			return Math.floor(value * 50) / 50;
+		case range <= 2:
+			return Math.floor(value * 20) / 20;
 		case range <= 4:
 			return Math.floor(value * 10) / 10;
 		case range <= 8:
@@ -13,6 +19,12 @@ const floor = (value, range) => {
 
 const ceil = (value, range) => {
 	switch (true) {
+		case range <= 0.4:
+			return Math.ceil(value * 100) / 100;
+		case range <= 0.8:
+			return Math.ceil(value * 50) / 50;
+		case range <= 2:
+			return Math.ceil(value * 20) / 20;
 		case range <= 4:
 			return Math.ceil(value * 10) / 10;
 		case range <= 8:
