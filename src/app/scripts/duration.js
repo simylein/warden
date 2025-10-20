17372f649d1243fa8253afa7543fff5e393b1161
simylein <67859356+simylein@users.noreply.github.com>
2025-10-20 16:57:22 +0200

    feat(device-buffers): use duration for human readability

---
+++ src/app/scripts/duration.js
@@ -0,0 +1,16 @@
+const duration = (seconds) => {
+	switch (true) {
+		case seconds < 1:
+			return `${seconds.toFixed(1)}s`;
+		case seconds >= 1 && seconds < 60:
+			return `${seconds}s`;
+		case seconds >= 60 && seconds < 600:
+			return `${(seconds / 60).toFixed(1)}m`;
+		case seconds >= 600 && seconds < 3600:
+			return `${Math.floor(seconds / 60)}m`;
+		case seconds >= 3600 && seconds < 36000:
+			return `${(seconds / 3600).toFixed(1)}h`;
+		case seconds >= 36000:
+			return `${Math.floor(seconds / 3600)}h`;
+	}
+};
