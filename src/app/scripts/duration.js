fee5f37cb00b074118b372bcbcf1fb7f1afc91dc
simylein <67859356+simylein@users.noreply.github.com>
2025-10-21 08:37:50 +0200

    fix(duration): round all non fixed seconds

--- src/app/scripts/duration.js
+++ src/app/scripts/duration.js
@@ -3,14 +3,14 @@ const duration = (seconds) => {
 		case seconds < 1:
 			return `${seconds.toFixed(1)}s`;
 		case seconds >= 1 && seconds < 60:
-			return `${seconds}s`;
+			return `${Math.round(seconds)}s`;
 		case seconds >= 60 && seconds < 600:
 			return `${(seconds / 60).toFixed(1)}m`;
 		case seconds >= 600 && seconds < 3600:
-			return `${Math.floor(seconds / 60)}m`;
+			return `${Math.round(seconds / 60)}m`;
 		case seconds >= 3600 && seconds < 36000:
 			return `${(seconds / 3600).toFixed(1)}h`;
 		case seconds >= 36000:
-			return `${Math.floor(seconds / 3600)}h`;
+			return `${Math.round(seconds / 3600)}h`;
 	}
 };
