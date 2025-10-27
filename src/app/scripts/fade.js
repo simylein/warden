4ecf0e42181b063cdfce66a2988a861631f90cb9
simylein <67859356+simylein@users.noreply.github.com>
2025-10-27 18:54:14 +0100

    fix(fade): only fade real colors

--- src/app/scripts/fade.js
+++ src/app/scripts/fade.js
@@ -1,7 +1,12 @@
 const fade = (element) => {
 	for (let ind = element.classList.length - 1; ind >= 0; ind--) {
 		const cls = element.classList[ind];
-		if (cls.startsWith('text-') || cls.startsWith('dark:text-') || cls.startsWith('background-') || cls.startsWith('dark:background-')) {
+		if (
+			(cls.startsWith('text-') && cls.endsWith('0')) ||
+			(cls.startsWith('dark:text-') && cls.endsWith('0')) ||
+			(cls.startsWith('background-') && cls.endsWith('0')) ||
+			(cls.startsWith('dark:background-') && cls.endsWith('0'))
+		) {
 			element.classList.remove(cls);
 		}
 	}
