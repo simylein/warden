80af857c6859abbd43648bf17248684378418619
simylein <67859356+simylein@users.noreply.github.com>
2025-11-18 21:11:42 +0100

    feat(device): color rack type yellow

--- src/app/scripts/color-device.js
+++ src/app/scripts/color-device.js
@@ -1,5 +1,7 @@
 const colorDevice = (element, type) => {
 	switch (true) {
+		case type === 'rack':
+			return element.classList.add('text-yellow-600', 'dark:text-yellow-400', 'background-yellow-50', 'dark:background-yellow-950');
 		case type === 'indoor':
 			return element.classList.add('text-green-600', 'dark:text-green-400', 'background-green-50', 'dark:background-green-950');
 		case type === 'outdoor':
