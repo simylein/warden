465edafdec96204337e164c3b3ce8c7ffae3fa97
simylein <67859356+simylein@users.noreply.github.com>
2025-12-11 22:27:24 +0100

    fix(zone-title): use same schema as device for width

--- src/app/scripts/zone-title.js
+++ src/app/scripts/zone-title.js
@@ -1,17 +1,22 @@
 const loadingZoneTitle = (element) => {
-	loading(element, ['w-24', 'h-4.5'], [], '');
+	loading(element, ['w-24', 'h-4.5'], []);
+	fade(element.children[0]);
+	paint(element.children[0], [], [], '');
 };
 
 const paintZoneTitle = (element, response) => {
+	paint(element, [], ['w-24', 'h-4.5']);
 	if (response.headers.get('zone-name') !== null) {
-		fade(element);
-		paint(element, [], ['w-24', 'h-4.5'], response.headers.get('zone-name'));
+		fade(element.children[0]);
+		paint(element.children[0], [], [], response.headers.get('zone-name'));
 	} else {
-		paint(element, [], [], 'null');
-		colorNull(element);
+		paint(element.children[0], [], [], 'null');
+		colorNull(element.children[0]);
 	}
 };
 
 const errorZoneTitle = (element) => {
-	error(element, ['w-24', 'h-4.5'], [], '');
+	error(element, ['w-24', 'h-4.5'], []);
+	fade(element.children[0]);
+	paint(element.children[0], [], [], '');
 };
