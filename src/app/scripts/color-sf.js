c4bf3cbafc235c5532035bf757c80885c4f3658b
simylein <67859356+simylein@users.noreply.github.com>
2025-12-22 00:42:39 +0100

    feat(sf+tx-power): prefer power over danger colors

--- src/app/scripts/color-sf.js
+++ src/app/scripts/color-sf.js
@@ -1,16 +1,18 @@
 const colorSf = (element, sf) => {
 	switch (true) {
-		case sf === 7:
+		case sf === 6:
 			return element.classList.add('text-green-600', 'dark:text-green-400', 'background-green-50', 'dark:background-green-950');
+		case sf === 7:
+			return element.classList.add('text-emerald-600', 'dark:text-emerald-400', 'background-emerald-50', 'dark:background-emerald-950');
 		case sf === 8:
-			return element.classList.add('text-lime-600', 'dark:text-lime-400', 'background-lime-50', 'dark:background-lime-950');
+			return element.classList.add('text-teal-600', 'dark:text-teal-400', 'background-teal-50', 'dark:background-teal-950');
 		case sf === 9:
-			return element.classList.add('text-yellow-600', 'dark:text-yellow-400', 'background-yellow-50', 'dark:background-yellow-950');
+			return element.classList.add('text-cyan-600', 'dark:text-cyan-400', 'background-cyan-50', 'dark:background-cyan-950');
 		case sf === 10:
-			return element.classList.add('text-amber-600', 'dark:text-amber-400', 'background-amber-50', 'dark:background-amber-950');
+			return element.classList.add('text-sky-600', 'dark:text-sky-400', 'background-sky-50', 'dark:background-sky-950');
 		case sf === 11:
-			return element.classList.add('text-orange-600', 'dark:text-orange-400', 'background-orange-50', 'dark:background-orange-950');
+			return element.classList.add('text-blue-600', 'dark:text-blue-400', 'background-blue-50', 'dark:background-blue-950');
 		case sf === 12:
-			return element.classList.add('text-red-600', 'dark:text-red-400', 'background-red-50', 'dark:background-red-950');
+			return element.classList.add('text-indigo-600', 'dark:text-indigo-400', 'background-indigo-50', 'dark:background-indigo-950');
 	}
 };
--- src/app/scripts/color-tx-power.js
+++ src/app/scripts/color-tx-power.js
@@ -1,16 +1,18 @@
 const colorTxPower = (element, txPower) => {
 	switch (true) {
-		case txPower < 6:
+		case txPower < 4:
 			return element.classList.add('text-green-600', 'dark:text-green-400', 'background-green-50', 'dark:background-green-950');
-		case txPower >= 6 && txPower < 9:
-			return element.classList.add('text-lime-600', 'dark:text-lime-400', 'background-lime-50', 'dark:background-lime-950');
-		case txPower >= 9 && txPower < 12:
-			return element.classList.add('text-yellow-600', 'dark:text-yellow-400', 'background-yellow-50', 'dark:background-yellow-950');
+		case txPower >= 4 && txPower < 7:
+			return element.classList.add('text-emerald-600', 'dark:text-emerald-400', 'background-emerald-50', 'dark:background-emerald-950');
+		case txPower >= 7 && txPower < 10:
+			return element.classList.add('text-teal-600', 'dark:text-teal-400', 'background-teal-50', 'dark:background-teal-950');
+		case txPower >= 10 && txPower < 12:
+			return element.classList.add('text-cyan-600', 'dark:text-cyan-400', 'background-cyan-50', 'dark:background-cyan-950');
 		case txPower >= 12 && txPower < 14:
-			return element.classList.add('text-amber-600', 'dark:text-amber-400', 'background-amber-50', 'dark:background-amber-950');
+			return element.classList.add('text-sky-600', 'dark:text-sky-400', 'background-sky-50', 'dark:background-sky-950');
 		case txPower >= 14 && txPower < 16:
-			return element.classList.add('text-orange-600', 'dark:text-orange-400', 'background-orange-50', 'dark:background-orange-950');
+			return element.classList.add('text-blue-600', 'dark:text-blue-400', 'background-blue-50', 'dark:background-blue-950');
 		case txPower >= 16:
-			return element.classList.add('text-red-600', 'dark:text-red-400', 'background-red-50', 'dark:background-red-950');
+			return element.classList.add('text-indigo-600', 'dark:text-indigo-400', 'background-indigo-50', 'dark:background-indigo-950');
 	}
 };
