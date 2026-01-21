979d9ee1c09468ea0331a9797623c5b8be549651
simylein <67859356+simylein@users.noreply.github.com>
2026-01-21 20:57:14 +0100

    chore(uplink+downlink): no longer serve detail pages

--- src/app/serve.c
+++ src/app/serve.c
@@ -1,6 +1,4 @@
 #include "../api/device.h"
-#include "../api/downlink.h"
-#include "../api/uplink.h"
 #include "../api/user-device.h"
 #include "../api/user-zone.h"
 #include "../api/user.h"
@@ -536,58 +534,6 @@ void serve_zone_signals(octet_t *db, bwt_t *bwt, request_t *request, response_t 
 	}
 
 	serve(&page_zone_signals, response);
-}
-
-void serve_uplink(sqlite3 *database, bwt_t *bwt, request_t *request, response_t *response) {
-	uint8_t uuid_len = 0;
-	const char *uuid = param_find(request, 8, &uuid_len);
-	if (uuid_len != sizeof(*((uplink_t *)0)->id) * 2) {
-		warn("uuid length %hhu does not match %zu\n", uuid_len, sizeof(*((uplink_t *)0)->id) * 2);
-		response->status = 400;
-		return;
-	}
-
-	uint8_t id[16];
-	if (base16_decode(id, sizeof(id), uuid, uuid_len) != 0) {
-		warn("failed to decode uuid from base 16\n");
-		response->status = 400;
-		return;
-	}
-
-	uplink_t uplink = {.id = &id};
-	uint16_t status = uplink_existing(database, bwt, &uplink);
-	if (status != 0) {
-		response->status = status;
-		return;
-	}
-
-	serve(&page_uplink, response);
-}
-
-void serve_downlink(sqlite3 *database, bwt_t *bwt, request_t *request, response_t *response) {
-	uint8_t uuid_len = 0;
-	const char *uuid = param_find(request, 10, &uuid_len);
-	if (uuid_len != sizeof(*((downlink_t *)0)->id) * 2) {
-		warn("uuid length %hhu does not match %zu\n", uuid_len, sizeof(*((downlink_t *)0)->id) * 2);
-		response->status = 400;
-		return;
-	}
-
-	uint8_t id[16];
-	if (base16_decode(id, sizeof(id), uuid, uuid_len) != 0) {
-		warn("failed to decode uuid from base 16\n");
-		response->status = 400;
-		return;
-	}
-
-	downlink_t downlink = {.id = &id};
-	uint16_t status = downlink_existing(database, bwt, &downlink);
-	if (status != 0) {
-		response->status = status;
-		return;
-	}
-
-	serve(&page_downlink, response);
 }
 
 void serve_user(octet_t *db, request_t *request, response_t *response) {
