ea2d6286ef03d28dc34849e0e692eeead91d87d4
simylein <67859356+simylein@users.noreply.github.com>
2026-01-21 18:17:53 +0100

    fix(init): set correct umask for created files

--- src/api/init.c
+++ src/api/init.c
@@ -1,5 +1,5 @@
+#include "../lib/error.h"
 #include "../lib/logger.h"
-#include "../lib/octet.h"
 #include "device.h"
 #include "user-device.h"
 #include "user-zone.h"
@@ -9,121 +9,108 @@
 #include <stdio.h>
 
 int init_user(octet_t *db) {
-	int status;
-
 	char file[128];
 	if (sprintf(file, "%s/%s.data", db->directory, user_file) == -1) {
 		error("failed to sprintf to file\n");
-		return 500;
+		return -1;
 	}
 
-	octet_stmt_t stmt;
-	if (octet_open(&stmt, file, O_CREAT | O_EXCL, F_RDLCK) == -1) {
-		status = -1;
-		goto cleanup;
+	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		error("failed to create file %s because %s\n", file, errno_str());
+		return -1;
 	}
 
 	info("created file %s\n", user_file);
-	status = 0;
 
-cleanup:
-	octet_close(&stmt, file);
-	return status;
+	close(fd);
+	return 0;
 }
 
 int init_device(octet_t *db) {
-	int status;
-
 	char file[128];
 	if (sprintf(file, "%s/%s.data", db->directory, device_file) == -1) {
 		error("failed to sprintf to file\n");
-		return 500;
+		return -1;
 	}
 
-	octet_stmt_t stmt;
-	if (octet_open(&stmt, file, O_CREAT | O_EXCL, F_RDLCK) == -1) {
-		status = -1;
-		goto cleanup;
+	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		error("failed to create file %s because %s\n", file, errno_str());
+		return -1;
 	}
 
 	info("created file %s\n", device_file);
-	status = 0;
 
-cleanup:
-	octet_close(&stmt, file);
-	return status;
+	close(fd);
+	return 0;
 }
 
 int init_user_device(octet_t *db) {
-	int status;
-
 	char file[128];
 	if (sprintf(file, "%s/%s.data", db->directory, user_device_file) == -1) {
 		error("failed to sprintf to file\n");
-		return 500;
+		return -1;
 	}
 
-	octet_stmt_t stmt;
-	if (octet_open(&stmt, file, O_CREAT | O_EXCL, F_RDLCK) == -1) {
-		status = -1;
-		goto cleanup;
+	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		error("failed to create file %s because %s\n", file, errno_str());
+		return -1;
 	}
 
 	info("created file %s\n", user_device_file);
-	status = 0;
 
-cleanup:
-	octet_close(&stmt, file);
-	return status;
+	close(fd);
+	return 0;
 }
 
 int init_zone(octet_t *db) {
-	int status;
-
 	char file[128];
 	if (sprintf(file, "%s/%s.data", db->directory, zone_file) == -1) {
 		error("failed to sprintf to file\n");
-		return 500;
+		return -1;
 	}
 
-	octet_stmt_t stmt;
-	if (octet_open(&stmt, file, O_CREAT | O_EXCL, F_RDLCK) == -1) {
-		status = -1;
-		goto cleanup;
+	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		error("failed to create file %s because %s\n", file, errno_str());
+		return -1;
 	}
 
 	info("created file %s\n", zone_file);
-	status = 0;
 
-cleanup:
-	octet_close(&stmt, file);
-	return status;
+	close(fd);
+	return 0;
 }
 
 int init_user_zone(octet_t *db) {
-	int status;
-
 	char file[128];
 	if (sprintf(file, "%s/%s.data", db->directory, user_zone_file) == -1) {
 		error("failed to sprintf to file\n");
-		return 500;
+		return -1;
 	}
 
-	octet_stmt_t stmt;
-	if (octet_open(&stmt, file, O_CREAT | O_EXCL, F_RDLCK) == -1) {
-		status = -1;
-		goto cleanup;
+	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if (fd == -1) {
+		error("failed to create file %s because %s\n", file, errno_str());
+		return -1;
 	}
 
 	info("created file %s\n", user_zone_file);
-	status = 0;
 
-cleanup:
-	octet_close(&stmt, file);
-	return status;
+	close(fd);
+	return 0;
 }
 
 int init(octet_t *db) {
+	if (mkdir(db->directory, S_IRUSR | S_IWUSR | S_IXUSR) == -1) {
+		error("failed to create directory %s because %s\n", db->directory, errno_str());
+		return -1;
+	}
+
+	info("created directory %s\n", db->directory);
+
 	if (init_user(db) == -1) {
 		return -1;
 	}
