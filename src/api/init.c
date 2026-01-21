b550fa532522b1cd34cfc5b59be0d9bacd888712
simylein <67859356+simylein@users.noreply.github.com>
2026-01-21 18:50:28 +0100

    feat(octet): support creating directories and files

--- src/api/init.c
+++ src/api/init.c
@@ -1,4 +1,3 @@
-#include "../lib/error.h"
 #include "../lib/logger.h"
 #include "device.h"
 #include "user-device.h"
@@ -15,15 +14,11 @@ int init_user(octet_t *db) {
 		return -1;
 	}
 
-	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		error("failed to create file %s because %s\n", file, errno_str());
+	if (octet_creat(file) == -1) {
 		return -1;
 	}
 
 	info("created file %s\n", user_file);
-
-	close(fd);
 	return 0;
 }
 
@@ -34,15 +29,11 @@ int init_device(octet_t *db) {
 		return -1;
 	}
 
-	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		error("failed to create file %s because %s\n", file, errno_str());
+	if (octet_creat(file) == -1) {
 		return -1;
 	}
 
 	info("created file %s\n", device_file);
-
-	close(fd);
 	return 0;
 }
 
@@ -53,15 +44,11 @@ int init_user_device(octet_t *db) {
 		return -1;
 	}
 
-	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		error("failed to create file %s because %s\n", file, errno_str());
+	if (octet_creat(file) == -1) {
 		return -1;
 	}
 
 	info("created file %s\n", user_device_file);
-
-	close(fd);
 	return 0;
 }
 
@@ -72,15 +59,11 @@ int init_zone(octet_t *db) {
 		return -1;
 	}
 
-	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		error("failed to create file %s because %s\n", file, errno_str());
+	if (octet_creat(file) == -1) {
 		return -1;
 	}
 
 	info("created file %s\n", zone_file);
-
-	close(fd);
 	return 0;
 }
 
@@ -91,21 +74,16 @@ int init_user_zone(octet_t *db) {
 		return -1;
 	}
 
-	int fd = open(file, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		error("failed to create file %s because %s\n", file, errno_str());
+	if (octet_creat(file) == -1) {
 		return -1;
 	}
 
 	info("created file %s\n", user_zone_file);
-
-	close(fd);
 	return 0;
 }
 
 int init(octet_t *db) {
-	if (mkdir(db->directory, S_IRUSR | S_IWUSR | S_IXUSR) == -1) {
-		error("failed to create directory %s because %s\n", db->directory, errno_str());
+	if (octet_mkdir(db->directory) == -1) {
 		return -1;
 	}
 
